1e18e9e150c3b16088546301c381ddab7c20405e
Soshi <migbonamim@gmail.com>
2022-11-07 20:21:41 -0300

    Sidebar Final

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,6 @@
+import { PencilLine } from 'phosphor-react'
+
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -8,12 +11,14 @@ export function Sidebar() {
           />
         
           <div className={styles.profile}>
+            <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/71841316?v=4" alt="" />
             <strong>Miguel Matias</strong>
             <span>Web Developer</span>
         </div>
 
         <footer>
           <a href='#'>
+            <PencilLine size={20}/>
             Editar seu perfil
           </a>
         </footer>
