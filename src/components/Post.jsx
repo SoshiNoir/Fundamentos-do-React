b55d1a3f4d82a725986817789225a023451fdf22
Soshi <migbonamim@gmail.com>
2022-11-08 13:58:26 -0300

    Avatar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
@@ -6,10 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://github.com/soshiNoir.png"
-          />
+          <Avatar src="https://github.com/soshiNoir.png" />
           <div className={styles.authorInfo}>
             <strong>Miguel Matias</strong>
             <span>Web Developer</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
 
 import styles from "./Sidebar.module.css";
 
@@ -11,11 +12,8 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/soshiNoir.png"
-          alt=""
-        />
+        <Avatar src="https://github.com/SoshiNoir.png" />
+
         <strong>Miguel Matias</strong>
         <span>Web Developer</span>
       </div>
